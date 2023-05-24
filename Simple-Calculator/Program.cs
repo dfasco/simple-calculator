4def615031a29a2fb866b0cff548eba68d1c36ee
dfasco <daniel.fasco84@gmail.com>
2023-05-23 22:32:39 -0400

    Add program logic

---
+++ Simple-Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number: ");
+
+var input = Console.ReadLine();
+int num1 = int.Parse(input);
+
+Console.WriteLine("Input the second number: ");
+
+input = Console.ReadLine();
+int num2 = int.Parse(input);
+
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[A]dd numbers");
+Console.WriteLine("[S]ubstract numbers");
+Console.WriteLine("[M]ultiply numbers");
+
+var userChoice = Console.ReadLine();
+
+switch(userChoice)
+{
+    case ("A"):
+    case ("a"):
+        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        break;
+    case ("S"):
+    case ("s"):
+        Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+        break;
+    case ("M"):
+    case ("m"):
+        Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+        break;
+    default:
+        Console.WriteLine("Invalid choice!");
+        break;
+}
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
+
